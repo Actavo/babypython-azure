65f30bb2ec07bd9ffeb4ef70719fef004189e649
Ben Chadwick <ben.chadwick@dublin.siteserv.ie>
2017-02-23 10:08:06 +0000

    init and views

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -3,7 +3,6 @@ from flask_restful import Resource, Api, reqparse
 from flask_pymongo import PyMongo
 from os import environ
 
-app = Flask(__name__)
 
 app.config['MONGO_URI'] = "mongodb://isiemphours:3A9cNCEUYSK6Ut7lP8HHVCx8bvKJPnTnmgCnU8VsYhb5w5T9etfxdPorxYHj17kYnh74dtLl7709M3T8uV4C1Q==@isiemphours.documents.azure.com:10250/people?ssl=true&ssl_cert_reqs=CERT_NONE"
 
